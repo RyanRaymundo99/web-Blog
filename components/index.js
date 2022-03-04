302da6dea82d2b0fff6971d62eecefe43e7a999f
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-03-03 17:50:31 -0800

    first commit

---
+++ components/index.js
@@ -0,0 +1,10 @@
+export { default as Author} from './Author'
+export { default as Categories } from './Categories';
+export { default as Comments} from './Comments'
+export { default as CommentsForm} from './CommentsForm'
+export { default as FeaturedPostsCard} from './FeaturedPostsCard'
+export { default as PostCard } from './PostCart';
+export { default as PostDetail} from './PostDetail'
+export { default as PostWidget } from './PostWidget';
+export { default as Header } from './Header';
+export { default as Layout} from './Layout'
