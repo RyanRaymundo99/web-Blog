f198a2b65c43ed65c7d067ce3a59b8903a0ee485
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-03-03 18:30:48 -0800

    enter

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -15,7 +15,7 @@ const Categories = () => {
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">Categories</h3>
       {categories.map((category) => (
-        <Link key={category.slug} href={`/category/${category.slug}`}>
+        <Link key={category} href={`/category/${category.slug}`}>
           <span className={`cursor-pointer block pb-3 mb-3`}>{category.name}</span>
         </Link>
       ))}
