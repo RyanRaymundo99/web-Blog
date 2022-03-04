b831cf3763f46881ad530e5204f6498a00b45c68
Ryan Raymundo <90113061+RyanRaymundo99@users.noreply.github.com>
2022-03-03 18:21:59 -0800

    enter

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 
-const Author = ({ author }) => {
+
+
+const Author = ({author} : { author: any}) => {
   return (
     <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
